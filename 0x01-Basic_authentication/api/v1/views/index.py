53f0c525dc1ecec0d7f3639b57b918ecbc870f86
HamzaEsadik <hamza.esadik.m@gmail.com>
2024-07-11 09:05:18 +0100

    all tasks

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-""" Module of Index views
+"""
+Module of Index views
 """
 from flask import jsonify, abort
 from api.v1.views import app_views
@@ -7,7 +8,8 @@ from api.v1.views import app_views
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
-    """ GET /api/v1/status
+    """
+    GET /api/v1/status
     Return:
       - the status of the API
     """
@@ -16,7 +18,8 @@ def status() -> str:
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
-    """ GET /api/v1/stats
+    """
+    GET /api/v1/stats
     Return:
       - the number of each objects
     """
@@ -24,3 +27,27 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route("/unauthorized/",
+                 strict_slashes=False)
+def unauthorized() -> str:
+    """
+    Route unauthorized requests
+
+    Returns:
+        str: 401 status
+    """
+    abort(401)
+
+
+@app_views.route("/forbidden/",
+                 strict_slashes=False)
+def forbidden() -> str:
+    """
+    Route forbidden requests
+
+    Returns:
+        str: 403 status
+    """
+    abort(403)
